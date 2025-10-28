94b1da38fa7d8401f298c4b236923b4b539e7540
BoW91 <dscatoha@gmail.com>
2025-10-28 12:01:45 +0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+  css: ['~/assets/scss/main.scss'],
+  app: {
+    head: {
+      title: 'PizzaGrad - Лучшая пицца в городе',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Горячая пицца на заказ с доставкой. Прямо из печи!' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+        }
+      ]
+    }
+  }
+})
